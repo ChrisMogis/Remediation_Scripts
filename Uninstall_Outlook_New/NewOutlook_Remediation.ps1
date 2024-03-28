f200ac034715179af5e74d2450b00d3a805a1df4
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2024-03-28 13:16:27 +0100

    Add files via upload

---
+++ Uninstall_Outlook_New/NewOutlook_Remediation.ps1
@@ -0,0 +1,18 @@
+﻿<#
+Version: 1.0
+Author: Christopher Mogis
+Description : Uninstall Outlook (New) and add registry key for hide New Outlook toggle in Outlook Desktop
+#> 
+
+try{
+    #Uninstall Microsoft Outlook New
+    Get-AppxPackage -Name *OutlookForWindows* | Remove-AppxPackage -ErrorAction stop
+    Write-Host "Microsoft Outlook (New) successfully removed."
+
+    #Disable New Outlook Toggle
+    New-ItemProperty -Path "HKCU:\\SOFTWARE\\Policies\\Microsoft\\Office\\16.0\\Outlook\\Options\\General" -Name "HideNewOutlookToggle" -Value "1"  -PropertyType "ExpandString"
+
+}
+catch{
+    Write-Error "Error removing Microsoft Outlook (New)."
+}
